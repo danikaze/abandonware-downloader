642cbed79e80dbc723a436155dd281240b7b8cab
danikaze <danikaze@gmail.com>
2019-03-08 18:34:40 +0900

    Provide different discover strategies for my-abandonware.com index pages

---
+++ src/pages/my-abandonware-com/index-strategies.ts
@@ -0,0 +1,55 @@
+import { getIndexPageClass } from './get-index-page-class';
+
+export const NAME_INITIALS = '$0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+export const YEARS = (() => {
+  // tslint:disable:no-magic-numbers
+  const years: string[] = [];
+  for (let y = 1978; y < 2018; y++) {
+    years.push(String(y));
+  }
+  return years;
+})();
+export const PLATFORMS = [
+  'amiga',
+  'amiga-cd32',
+  'cpc',
+  'apple2',
+  'apple2gs',
+  'atari-8-bit',
+  'atari-st',
+  'colecovision',
+  'commodore-16-plus4',
+  'c64',
+  'dos',
+  'dragon-3264',
+  'game-gear',
+  'genesis',
+  'linux',
+  'mac',
+  'msx',
+  'pc88',
+  'pc98',
+  'sega-32x',
+  'sega-cd',
+  'sega-master-system',
+  'vic-20',
+  'windows',
+  'win3x',
+  'zx-spectrum',
+];
+export const GENRES = [
+  'action-1',
+  'adventure-2',
+  'educational-3',
+  'puzzle-9',
+  'racing-4',
+  'rpg-5',
+  'simulation-7',
+  'sports-8',
+  'strategy-6',
+];
+
+export const IndexName = getIndexPageClass('Name', 'name', NAME_INITIALS);
+export const IndexYear = getIndexPageClass('Year', 'year', YEARS);
+export const IndexPlatform = getIndexPageClass('Platform', 'platform', PLATFORMS);
+export const IndexGenre = getIndexPageClass('Genre', 'genre', GENRES);
