6f9f2b46f82e5946d4c1c6354e1e9d0c04286524
danikaze <danikaze@gmail.com>
2019-03-11 17:23:35 +0900

    Fix: Clean forbidden values in customizable paths

--- src/utils/path-builder.ts
+++ src/utils/path-builder.ts
@@ -4,15 +4,19 @@ import { getSettings } from './settings';
 
 export type Path = 'gameDownloads' | 'gameScreenshots' | 'gameInfo' | 'internalData';
 
+function safeValue(str: string): string {
+  return str.replace(/[\///|]/g, '-');
+}
+
 export function pathBuilder(path: Path, info: Partial<GameInfo> = {}): string {
   const settings = getSettings();
   const values = {
     root: join(__dirname, '..', '..'),
-    name: info.name || '',
-    platform: info.platform || '',
-    year: info.year || '',
-    genre: info.meta && info.meta.genre || '',
-    publisher: info.meta && info.meta.publisher || '',
+    name: safeValue(info.name || ''),
+    platform: safeValue(info.platform || ''),
+    year: safeValue(String(info.year) || ''),
+    genre: safeValue(info.meta && info.meta.genre || ''),
+    publisher: safeValue(info.meta && info.meta.publisher || ''),
   };
 
   const link = Object.keys(values).reduce((str, key) => {
